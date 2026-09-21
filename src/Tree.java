79c1d9ecc01066e86cb39bde1a418fa40524f639
Hongbo Wang <andrewwang703@gmail.com>
2026-09-21 14:31:12 -0400

    tree

--- src/Tree.java
+++ src/Tree.java
@@ -1,6 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tree {
     // We recommend attempting this class last, as it hasn't been scaffolded for your team.
     // Even if your team doesn't have time to implement this class, it is a useful exercise
     // to think about how you might split up the work to get the Tree and TreeMultiSet
     // implemented.
+    private Integer root;
+    private List<Tree> subtrees = new ArrayList<Tree>();
+
+    public Tree(int root, List<Tree> subtrees) {
+        this.root = root;
+        if (subtrees == null) {
+            this.subtrees = new ArrayList<Tree>();
+        } else {
+            this.subtrees = subtrees;
+        }
+    }
+
+    public boolean is_empty() {
+        return this.root == null;
+    }
+
+    public int length(){
+        if (this.is_empty()) {
+            return 0;
+        } else{
+            int size = 0;
+            for (Tree subtree : this.subtrees){
+                size += subtree.length();
+            }
+            return size;
+
+        }
+    }
+    public int count(int item){
+        if (this.is_empty()){
+            return 0;
+        } else {
+            int num = 0;
+            if (this.root == item){
+                num += 1;
+            }
+            for (Tree subtree : this.subtrees){
+                num += subtree.count(item);
+            }
+            return num;
+        }
+    }
+
 }
